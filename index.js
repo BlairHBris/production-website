804ac843fe32854d0411b55fc72cf219088993e2
Blair Bristow <blairhbristow@gmail.com>
2022-04-06 16:01:17 -0400

    Begin to integrate forms and buttons

---
+++ index.js
@@ -0,0 +1,12 @@
+const toTopButton = document.querySelector("#backToTop")
+const startingSubmissionButton = document.querySelector("#startingSubmission")
+
+function backToTop() {
+    document.documentElement.scrollTop = 0;
+}
+
+document.querySelector("#backToTop").onclick = backToTop
+
+function travel(){
+    
+}
